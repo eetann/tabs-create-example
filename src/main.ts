8a9aaeb3780e54db46d46f77e3bf55fe15d7e802
eetann <hideharu092@gmail.com>
2022-10-15 16:47:01 +0900

    add ESLint and Prettier

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
-import './style.css'
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import "./style.css";
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const app = document.querySelector<HTMLDivElement>("#app")!;
 
 app.innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
   <button>Count: 0</button>
-`
+`;
 
-let count = 0
-const button = document.querySelector('#app button')!
-button.addEventListener('click', () => {
-  count++
-  button.textContent = `Count: ${count}`
-})
+let count = 0;
+const button = document.querySelector("#app button")!;
+button.addEventListener("click", () => {
+  count++;
+  button.textContent = `Count: ${count}`;
+});
--- src/manifest.ts
+++ src/manifest.ts
@@ -6,5 +6,5 @@ export default defineManifest({
   description: "chrome.tabs example",
   version: "1.0.0",
   permissions: [],
-  action: { "default_popup": "index.html" }
+  action: { default_popup: "index.html" },
 });
